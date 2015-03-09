637cd2f56db07d55aebb5bafd573f6a148fafd2e
Ivan Malykh <ivan@lesslines.com>
2015-03-09 22:55:32 +0100

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
     address:              'smtp.mandrillapp.com',
     port:                 587,                     # ports 587 and 2525 are also supported with STARTTLS
     enable_starttls_auto: true,                    # detects and uses STARTTLS
-    user_name:            ENV['MANDRIL_USERNAME'],
-    password:             ENV['MANDRIL_PASSWORD'], # SMTP password is any valid API key
+    user_name:            ENV['MANDRILL_USERNAME'],
+    password:             ENV['MANDRILL_PASSWORD'], # SMTP password is any valid API key
     authentication:       'login',                 # Mandrill supports 'plain' or 'login'
     domain:               'onsbasisinkomen.nl',    # your domain to identify your server when connecting
     from:                 "\"Ons Basisinkomen\" <hallo@onsbasisinkomen.nl>"
